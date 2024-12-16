96b8cef2666175114c220a7e12d8b3cf0e90b575
Alessandro Tuveri <at@specialistaweb.it>
2024-12-16 15:01:48 +0100

    compatibility with TYPO3 v. 13
    Compatibility with TYPO3 v. 13 - added some files to configure a site via Site Management

--- ext_tables.php
+++ ext_tables.php
@@ -14,8 +14,8 @@ defined('TYPO3') or die();
 
         \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('rollovercollection3', 'Configuration/TypoScript', 'OHE effects style1-10');
 
-
-	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScript('rollovercollection3', 'setup', '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:rollovercollection3/Configuration/TypoScript/setup.ts>"');
+	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScript('rollovercollection3', 'constants', '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:rollovercollection3/Configuration/TypoScript/constants.typoscript>"');
+	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScript('rollovercollection3', 'setup', '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:rollovercollection3/Configuration/TypoScript/setup.typoscript>"');
 
 
 })();
